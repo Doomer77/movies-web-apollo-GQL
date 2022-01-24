6a6dea901ef9b7f6b5c9daacc1bb164b5edc5a60
Doomer77 <newzibkoff1984@gmail.com>
2022-01-24 21:20:21 +0300

    addeded components Details Home Movie

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -1,1 +1,73 @@
-export default () => "О фильме";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+      rating
+      language
+    }
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Poster = styled.div`
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: {
+      id,
+    },
+  });
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? "Загрузка..." : data?.movie?.title}</Title>
+        <Subtitle>
+          {data?.movie?.language} · {data?.movie?.rating}
+        </Subtitle>
+        <Description>{data?.movie?.description_intro}</Description>
+      </Column>
+      <Poster bg={data?.movie?.medium_cover_image}></Poster>
+    </Container>
+  );
+};
